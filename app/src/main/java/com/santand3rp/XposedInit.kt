a9d9d8d1cfa90b1df1afdf3cc9894b21e5c35c9f
mwilky <mwilkinsonm@gmail.com>
2025-04-17 16:10:42 +0100

    Added the string decoder hooks used for debugging

--- app/src/main/java/com/santand3rp/XposedInit.kt
+++ app/src/main/java/com/santand3rp/XposedInit.kt
@@ -45,6 +45,131 @@ class XposedInit : IXposedHookLoadPackage, IXposedHookZygoteInit {
                         }
                     }
                 )
+
+                /*
+                 The below hooks are hooking the string decoders in the app. This makes the string readable so we can
+                 determine what the app is searching for in regards to root/zygisk. You can filter out results here
+                 for things like "su" "root" "zygisk" "zygote" etc if required. Only uncomment these to debug.
+                */
+
+                /*
+                findAndHookMethod(
+                    "aat.\u0864\u0868",
+                    lpparam.classLoader,
+                    "\u1ad6",
+                    String::class.java, Short::class.javaPrimitiveType, Short::class.javaPrimitiveType,
+                    object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            val result = param.result?.toString() ?: return
+                            log("🔍 Decoded: $result")
+                        }
+                    }
+                )
+
+                findAndHookMethod(
+                    "aat.\u0865\u0868",
+                    lpparam.classLoader,
+                    "\u086a",
+                    String::class.java, Short::class.javaPrimitiveType, Short::class.javaPrimitiveType,
+                    object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            val result = param.result?.toString() ?: return
+                            log("🔍 Decoded: $result")
+                        }
+                    }
+                )
+
+                findAndHookMethod(
+                    "aat.\u086b\u0868",
+                    lpparam.classLoader,
+                    "\u1ada",
+                    String::class.java, Short::class.javaPrimitiveType, Short::class.javaPrimitiveType,
+                    object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            val result = param.result?.toString() ?: return
+                            log("🔍 Decoded: $result")
+                        }
+                    }
+                )
+
+                findAndHookMethod(
+                    "aat.\u1ac5\u0868",
+                    lpparam.classLoader,
+                    "\u1ac9",
+                    String::class.java, Short::class.javaPrimitiveType, Short::class.javaPrimitiveType,
+                    object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            val result = param.result?.toString() ?: return
+                            log("🔍 Decoded: $result")
+                        }
+                    }
+                )
+
+                findAndHookMethod(
+                    "aat.\u1acb\u0868",
+                    lpparam.classLoader,
+                    "\u086f",
+                    String::class.java, Short::class.javaPrimitiveType, Short::class.javaPrimitiveType,
+                    object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            val result = param.result?.toString() ?: return
+                            log("🔍 Decoded: $result")
+                        }
+                    }
+                )
+
+                findAndHookMethod(
+                    "aat.\u1ad0\u0868",
+                    lpparam.classLoader,
+                    "\u086b",
+                    String::class.java, Short::class.javaPrimitiveType, Short::class.javaPrimitiveType,
+                    object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            val result = param.result?.toString() ?: return
+                            log("🔍 Decoded: $result")
+                        }
+                    }
+                )
+
+                findAndHookMethod(
+                    "aat.\u1ad0\u0868",
+                    lpparam.classLoader,
+                    "\u086b",
+                    String::class.java, Short::class.javaPrimitiveType, Short::class.javaPrimitiveType,
+                    object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            val result = param.result?.toString() ?: return
+                            log("🔍 Decoded: $result")
+                        }
+                    }
+                )
+
+                findAndHookMethod(
+                    "aat.\u1ad2\u0868",
+                    lpparam.classLoader,
+                    "\u086c",
+                    String::class.java, Short::class.javaPrimitiveType, Short::class.javaPrimitiveType,
+                    object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            val result = param.result?.toString() ?: return
+                            log("🔍 Decoded: $result")
+                        }
+                    }
+                )
+
+                findAndHookMethod(
+                    "aat.\u1ad5\u0868",
+                    lpparam.classLoader,
+                    "\u0863",
+                    String::class.java, Short::class.javaPrimitiveType, Short::class.javaPrimitiveType,
+                    object : XC_MethodHook() {
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            val result = param.result?.toString() ?: return
+                            log("🔍 Decoded: $result")
+                        }
+                    }
+                )
+                */
             }
         }
     }
